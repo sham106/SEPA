18012da3ebd66ce89b8f7de43f556647beffc476
sham106 <75040811+sham106@users.noreply.github.com>
2022-05-17 22:55:40 +0300

    Add files via upload

---
+++ Number4.py
@@ -0,0 +1,12 @@
+#   Print the numbers that are divisible by the sum of its digits
+for count in range(1, 101):
+
+    # Finding the sum of the digits of each number
+    sum_of_count = 0
+    string_number = str(count)
+    for i in string_number:
+        sum_of_count += int(i)
+
+# Determining if the sum of the  digits divides the number wholly
+    if count % sum_of_count == 0:
+        print(count)
