666ce287538a55b642416aa667be32e2a57518f2
sham106 <75040811+sham106@users.noreply.github.com>
2022-05-18 07:15:55 +0300

    Add files via upload

---
+++ names1.py
@@ -0,0 +1,10 @@
+#  opening the file “names.txt” and print its contents using a loop
+def searchname():
+    file = open("names.txt" , "r")
+    for s in infile:
+        s = file.readlines()
+
+        print(s)
+
+
+
