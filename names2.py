ced49973057eb04d312767dab3cbc6cf1b5a4c62
sham106 <75040811+sham106@users.noreply.github.com>
2022-05-20 15:34:46 +0300

    Add files via upload

--- names2.py
+++ names2.py
@@ -1,6 +1,9 @@
 def searchname():
-    infile = open("names.txt" , "r")
+    infile = open("names.txt", "r")
     for s in infile:
+        s = s.strip()
         if s.startswith("A"):
-
             print(s)
+
+
+searchname()
